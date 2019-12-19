c81e3d16ea70d35d34678e7f36ff7a0a1fd93567
João Abreu <tinoco@mobixtec.com>
2019-12-19 19:05:40 -0300

    Adding new host

--- server/deploy.js
+++ server/deploy.js
@@ -1,6 +1,6 @@
 express = require('express');
 exec = require('child_process').exec;
 
-exec('json-server --watch --host mobix-react-redux.herokuapp.com --delay 3000 ./server/api.json --port 80', function(req, res, next) {
+exec('json-server --watch --host 127.0.0.1 --delay 3000 ./server/api.json --port 80', function(req, res, next) {
     console.log(req);
 });
