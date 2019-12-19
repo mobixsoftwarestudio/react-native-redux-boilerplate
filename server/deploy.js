7efd4d34c094698e9ffb499e2ee1aca62fce580b
João Abreu <tinoco@mobixtec.com>
2019-12-19 19:00:45 -0300

    Minor changes

--- server/deploy.js
+++ server/deploy.js
@@ -1,4 +1,6 @@
 express = require('express');
 exec = require('child_process').exec;
 
-exec('json-server --watch --host mobix-react-redux.herokuapp.com --delay 3000 ./server/api.json --port 80');
+exec('json-server --watch --host mobix-react-redux.herokuapp.com --delay 3000 ./server/api.json --port 80', function(req, res, next) {
+    console.log(req);
+});
