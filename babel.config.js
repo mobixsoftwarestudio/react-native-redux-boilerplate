e07badd26ca803c81941dea3791a1a4f521dc610
Renato Rodrigues <renato@mobixtec.com>
2020-01-08 11:28:23 -0300

    add navigation libs + babel and tsconfig modules

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,36 @@
 module.exports = function(api) {
 	api.cache(true);
 
+	const plugins = [
+		[
+			'module-resolver',
+			{
+				root: ['./'],
+				extensions: [
+					'.ios.js',
+					'.android.js',
+					'.js',
+					'.json',
+					'.jsx',
+					'.ts',
+					'.tsx',
+				],
+				alias: {
+					'@app': './src/App',
+					'@assets': './src/assets',
+					'@styles': './src/styles',
+					'@core': './src/core',
+					'@utils': './src/utils',
+					'@modules': './src/modules',
+					'@store': './src/store',
+				},
+			},
+		],
+	];
 	const presets = ['module:metro-react-native-babel-preset'];
 
 	return {
+		plugins,
 		presets,
 		sourceMaps: true,
 	};
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src';
+import App from '@app';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
 
