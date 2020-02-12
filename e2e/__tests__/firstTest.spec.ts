67811319cb352afabba31bc2cf3ac6242a9ea054
Renato Rodrigues <renato@mobixtec.com>
2020-02-12 17:14:47 -0300

    tests with detox

---
+++ e2e/__tests__/firstTest.spec.ts
@@ -0,0 +1,18 @@
+import { by, device, element, expect } from 'detox';
+
+describe('Example', () => {
+	beforeEach(async () => {
+		await device.reloadReactNative();
+	});
+
+	it('should have welcome screen', async () => {
+		await expect(element(by.id('welcome'))).toBeVisible();
+	});
+
+	it('should show hello screen after tap', async () => {
+		await element(by.id('login-btn')).tap();
+		await expect(
+			element(by.text(`Main Screen!\nYou're Logged!`)),
+		).toBeVisible();
+	});
+});
---
+++ e2e/init.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import detox from 'detox';
+import adapter from 'detox/runners/jest/adapter';
+const specReporter = require('detox/runners/jest/specReporter');
+const config = require('../package.json').detox;
+import dotenv from 'dotenv';
+
+//to load the username and password specified in the .env
+dotenv.config();
+
+// Set the default timeout
+jest.setTimeout(120000);
+jasmine.getEnv().addReporter(adapter);
+
+// This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
+// This is strictly optional.
+jasmine.getEnv().addReporter(specReporter);
+
+beforeAll(async () => {
+	await detox.init(config);
+});
+
+beforeEach(async () => {
+	await adapter.beforeEach();
+});
+
+afterAll(async () => {
+	await adapter.afterAll();
+	await detox.cleanup();
+});
